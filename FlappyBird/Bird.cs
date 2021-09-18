1a193f9097408b1e7aa1140c53182af97b0f1739
Mikah <mikah@mltd.com>
2021-09-17 17:08:25 -0700

    did cool stuff

--- FlappyBird/Bird.cs
+++ FlappyBird/Bird.cs
@@ -12,31 +12,44 @@ namespace FlappyBird
 
         private bool wasMousePressed;
         public double SurvivalTime { get; set; }
+        public double ShouldJump { get; set; }
 
         public Bird(Texture2D tex, Color tint, Vector2 pos, Vector2 size, Vector2 scale, Vector2 origin)
             :base(tex, tint, pos, size, scale, origin)
         {
             wasMousePressed = false;
             SurvivalTime = 0;
+            ShouldJump = 0;
         }
 
         public override void Update(GameTime gameTime)
         {
-            if (InputManager.MouseState.LeftButton == ButtonState.Pressed)
+            //if (InputManager.MouseState.LeftButton == ButtonState.Pressed)
+            //{
+            //    wasMousePressed = true;
+            //}
+            //else if(wasMousePressed)
+            //{
+            //    if (Pos.Y - 100 > 0)
+            //    {
+            //        Pos = new Vector2(Pos.X, Pos.Y - 100);
+            //    }
+            //    else
+            //    {
+            //        Pos = new Vector2(Pos.X, 0);
+            //    }
+            //    wasMousePressed = false;
+            //}
+            if(ShouldJump > 0)
             {
-                wasMousePressed = true;
-            }
-            else if(wasMousePressed)
-            {
-                if (Pos.Y - 100 > 0)
-                {
-                    Pos = new Vector2(Pos.X, Pos.Y - 100);
-                }
-                else
-                {
-                    Pos = new Vector2(Pos.X, 0);
-                }
-                wasMousePressed = false;
+                    if (Pos.Y - 100 > 0)
+                    {
+                        Pos = new Vector2(Pos.X, Pos.Y - 100);
+                    }
+                    else
+                    {
+                        Pos = new Vector2(Pos.X, 0);
+                    }
             }
             Pos = new Vector2(Pos.X, Pos.Y + 2);
         }
--- FlappyBird/PlayScreen.cs
+++ FlappyBird/PlayScreen.cs
@@ -24,6 +24,7 @@ namespace FlappyBird
         public Bird[] Flappies;
         public int timeToNextPipe;
         public NeuralNet[] Nets;
+        public Pipe ClosestPipe;
         public PlayScreen(GraphicsDevice graphicsDevice, Vector2 screenSize)
             :base()
         {
@@ -43,28 +44,53 @@ namespace FlappyBird
             for (int i = 0; i < Flappies.Length; i ++)
             {
                 Flappies[i] = new Bird(Color.White.CreatePixel(graphicsDevice), Color.Green, screenSize / 2 - flappySize, Vector2.One, flappySize, Vector2.Zero);
-                Flappies[i].Pos = new Vector2(ScreenSize.X / 2 - Flappies[i].Size.X, ScreenSize.Y / 2 - Flappies[i].Size.Y - i * 2);
+                Flappies[i].Pos = new Vector2(ScreenSize.X / 5 - Flappies[i].Size.X, ScreenSize.Y / 2 - Flappies[i].Size.Y - i * 2);
                 GameObjects.Add(Flappies[i]);
 
                 Nets[i] = new NeuralNet(meanSquared, tanh, Layers);
-                Nets[i].Randomize(Random,);
+                Nets[i].Randomize(Random, -50, 50);
             }
             timeToNextPipe = 4000;
+            ClosestPipe = null;
         }
 
         public override void Update(GameTime gameTime)
         {
+            if(Pipes.Count > 0 && Pipes[0].Pos.X + Pipes[0].HitBox.Width < 0)
+            {
+                Pipes.RemoveAt(0);
+            }
+
             for(int i = 0; i < Flappies.Length; i ++)
             {
                 if(Flappies[i].IsVisible)
                 {
                     Flappies[i].SurvivalTime += gameTime.ElapsedGameTime.TotalSeconds;
+                    double[] inputs;
+                    if (ClosestPipe != null)
+                    {
+                        inputs = new double[]
+                        {
+                        ClosestPipe.Pos.X + ClosestPipe.HitBox.Width - Flappies[i].Pos.X,
+                        ClosestPipe.Pos.Y + ClosestPipe.SpaceBetweenPipes.Y * 3 / 4 - Flappies[i].Pos.Y,
+                        };
+
+                    }
+                    else
+                    {
+                        inputs = new double[]
+                        {
+                            100, 
+                            0,
+                        };
+                    }
+                    Flappies[i].ShouldJump = Nets[i].Compute(inputs)[0];
                 }
             }
 
             if(gameTime.TotalGameTime.TotalMilliseconds - LastElapsedTime > timeToNextPipe)
             {
-                timeToNextPipe = Random.Next(1500, 6000);
+                timeToNextPipe = Random.Next(1500, 4000);
                 LastElapsedTime = gameTime.TotalGameTime.TotalMilliseconds;
                 Pipe nextPipe = new Pipe(40, new Vector2(40, 150), ScreenSize, Random, GraphicsDevice);
                 Pipes.Add(nextPipe);
@@ -76,11 +102,20 @@ namespace FlappyBird
             for (int i = 0; i < Flappies.Length; i ++)
             {
                 bool shouldInvisible = false;
-                foreach (Pipe pipe in Pipes)
+                for(int x = 0; x < Pipes.Count; x ++ )
                 {
-                    if (Flappies[i].HitBox.Intersects(pipe.HitBox) || Flappies[i].HitBox.Intersects(pipe.OtherPart.HitBox))
+                    if (Flappies[i].HitBox.Intersects(Pipes[x].HitBox) || Flappies[i].HitBox.Intersects(Pipes[x].OtherPart.HitBox))
                     {
                         shouldInvisible = true;
+                    }
+
+                    if (ClosestPipe == null)
+                    {
+                        ClosestPipe = Pipes[0];
+                    }
+                    else if (ClosestPipe.Pos.X + ClosestPipe.HitBox.Width < Flappies[i].Pos.X)
+                    {
+                        ClosestPipe = Pipes[x + 1];
                     }
                 }
                 if (Flappies[i].Pos.Y + Flappies[i].HitBox.Height > ScreenSize.Y || shouldInvisible)
